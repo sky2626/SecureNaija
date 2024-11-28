91a50eeda8d0cd03380d0f25480e091b4e7d0cbc
Salu Alhassan <alhassansalu1@gmail.com>
2024-11-28 09:27:26 +0000

    update

--- components/mini.vue
+++ components/mini.vue
@@ -18,11 +18,12 @@
 
         <!-- Dropdown menu -->
         <div v-if="isOpen"
-            class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            class="origin-top-right absolute right-0 mt-2 w-46 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
                 <!-- Language Links -->
-                <NuxtLink to="/housa" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Housa</NuxtLink>
+                <NuxtLink to="/learn" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">English</NuxtLink>
+                <NuxtLink to="/housa" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Hausa</NuxtLink>
                 <NuxtLink to="/yoruba" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Yoruba</NuxtLink>
                 <NuxtLink to="/igbo" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Igbo</NuxtLink>
                 <NuxtLink to="/ful" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Fulfulde</NuxtLink>
--- components/specialfooter.vue
+++ components/specialfooter.vue
@@ -96,17 +96,13 @@
                 <div class="sm:flex sm:items-center sm:justify-start sm:space-x-8">
                     <ul class="flex items-center justify-start space-x-8">
                         <li>
-                            <a href="#" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
-                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                    <path
-                                        d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"
-                                    ></path>
-                                </svg>
+                            <a href="https://x.com/SecureNaija01?s=08" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"/></svg>
                             </a>
                         </li>
 
                         <li>
-                            <a href="#" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
+                            <a href="https://www.facebook.com/profile.php?id=61569502581302" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
                                 <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                     <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
                                 </svg>
@@ -114,7 +110,7 @@
                         </li>
 
                         <li>
-                            <a href="#" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
+                            <a href="https://www.instagram.com/securenaija01" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
                                 <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                     <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path>
                                     <circle cx="16.806" cy="7.207" r="1.078"></circle>
@@ -126,7 +122,7 @@
                         </li>
 
                         <li>
-                            <a href="#" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
+                            <a href="https://github.com/Jaloskid01" title="" class="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
                                 <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                     <path
                                         fill-rule="evenodd"
